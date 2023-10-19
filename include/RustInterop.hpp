bf25069905772db5051faa5d9009039e6e99df96
billy4479 <giachi.ellero@gmail.com>
2023-10-20 00:40:41 +0200

    Improve (?) error handling to avoid dealing with c++ exceptions

--- include/RustInterop.hpp
+++ include/RustInterop.hpp
@@ -4,3 +4,62 @@
 inline std::string_view to_string_view(::rust::Str rust_str) {
   return std::string_view(rust_str.data(), rust_str.length());
 }
+
+template <typename F> struct NoDiscard {
+  F f;
+  NoDiscard(F const &func) : f(func) {}
+  template <typename... T>
+  [[nodiscard]] constexpr auto operator()(T &&...t) const
+      noexcept(noexcept(f(std::forward<T>(t)...))) {
+    return f(std::forward<T>(t)...);
+  }
+};
+
+template <typename Ret, typename... Args>
+[[nodiscard]] inline constexpr auto to_variant(Ret (*function)(Args...)) {
+  if constexpr (std::is_same_v<Ret, void>) {
+    return NoDiscard(
+        [function](Args... args) -> std::optional<std::exception const *> {
+          try {
+            if constexpr (sizeof...(args))
+              function(std::forward(args...));
+            else
+              function();
+            return {};
+          } catch (std::exception const *e) {
+            return e;
+          }
+        });
+  } else {
+    return NoDiscard(
+        [function](Args... args) -> std::variant<Ret, std::exception const *> {
+          try {
+            if constexpr (sizeof...(args))
+              return function(std::forward(args...));
+            else
+              return function();
+          } catch (std::exception const *e) {
+            return e;
+          }
+        });
+  }
+}
+
+template <typename T> [[nodiscard]] inline constexpr auto must(T result) {
+  if constexpr (std::is_same_v<T, std::optional<std::exception const *>>) {
+    if (result.has_value()) {
+      fmt::println(stderr, "rust exception: {}", result.value()->what());
+      std::terminate();
+    }
+
+    return;
+  } else {
+    if (std::holds_alternative<const std::exception *>(result)) {
+      fmt::println(stderr, "rust exception: {}",
+                   std::get<const std::exception *>(result)->what());
+      std::terminate();
+    }
+
+    return std::move(std::get<0>(result));
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include "RustInterop.hpp"
 
 int main(int argc, char **argv) {
-  try_init_logger();
+  must(to_variant(try_init_logger)());
 
-  auto config = try_load_config();
-  auto dir = to_string_view(config_get_server_dir(*config.into_raw()));
+  auto config = must(to_variant(try_load_config)());
+
+  auto dir = to_string_view(config_get_server_dir(*config));
 
   log_info(fmt::format("Hello from C++! server_dir: {}", dir));
 
